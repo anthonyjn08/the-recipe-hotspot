f61e879ebe59e7f43f06433162bb77ffdf8f68a4
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-06-23 01:28:03 +0000

    Create superuser

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Recipe
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('ingredients', 'instructions')
