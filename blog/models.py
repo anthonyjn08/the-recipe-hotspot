3f541bf069b91f5e2f5033cdd0e125e67a23af60
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-06-23 00:38:47 +0000

    Add recipe model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_summernote.fields import SummernoteTextField
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                                related_name="recipes")
+    created_on = models.DateTimeField(auto_now=True)
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    ingredients = SummernoteTextField()
+    instructions = SummernoteTextField()
+    cooking_time = models.IntegerField()
+    updated_on = models.DateTimeField(auto_now=True)
+    meal_type = models.CharField(max_length=50)
+    meal_tags = models.CharField(max_length=50)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name="recipe_likes",
+                                blank=True)
