16f6e490e0debd3cf40bd45c3147399be87bc3b3
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-06-25 15:36:23 +0000

    Add post_detail.html

--- blog/views.py
+++ blog/views.py
@@ -14,7 +14,7 @@ class RecipeDetail(View):
     
     def get(self, request, slug, *args, **kwargs):
         queryset = Recipe.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
+        recipe = get_object_or_404(queryset, slug=slug)
         comments = recipe.comments.order_by('-created_on')
         liked = False
         if recipe.likes.filter(id=self.request.user.id).exists():
